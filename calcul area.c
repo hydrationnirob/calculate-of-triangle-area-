e594b0c19136ee417fbde6af2f2ab9d976e362a4
Md Rakibull Haque <52385810+hydrationnirob@users.noreply.github.com>
2022-06-05 13:22:45 +0600

    Add files via upload

---
+++ calcul area.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    float area; //colacule of circal
+
+    printf("enter redius of circal");
+    scanf("%f", area);
+
+    printf("your area is %f", 2*3.1416*area);
+    
+    return 0;
+}
